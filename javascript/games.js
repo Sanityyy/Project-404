0c7367f7fada6fa32b7b041d7cdbe9d21032a279
Sanity <stiannordholm@gmail.com>
2020-05-09 18:35:19 +0200

    Added games page

---
+++ javascript/games.js
@@ -0,0 +1,23 @@
+let db = firebase.database();
+let spill = db.ref("Spill/spill");
+let main = document.querySelector("main");
+
+function visAlleSpill() {
+    main.innerHTML = "";
+    spill.orderByChild("nummer").on("child_added", visSpill);
+}
+
+// function (typer spill???)
+
+function visSpill(snapshot) {
+    let spill = snapshot.val();
+    let spillNokkel = snapshot.key;
+
+    main.innerHTML +=   `
+    <div id="card" class="cards" ><h3>${spill.Navn} </h3>
+			 <h3>${spill.nummer} <br> ${spill.Creator} </h3>
+	
+		</div>`;
+}
+
+window.onload = visAlleSpill;
