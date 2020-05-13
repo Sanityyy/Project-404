03fe2a9d0eaa63e63f42298509bf7bfdc54d8e85
Sanity <stiannordholm@gmail.com>
2020-05-13 10:37:07 +0200

    brosjan

--- javascript/games.js
+++ javascript/games.js
@@ -15,7 +15,7 @@ function visSpill(snapshot) {
 
     main.innerHTML +=   `
     <div id="card" class="cards" ><h3>${spill.Navn} </h3>
-			 <h3>${spill.nummer} <br> ${spill.Creator} </h3>
+			 <h3><img src="${spill.picture}" alt="${spill.Navn}" width="100%" height="100%"> <br> ${spill.Creator} </h3>
 	
 		</div>`;
 }
