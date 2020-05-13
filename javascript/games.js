971972e463f924d86855500bfa841b365be7ca37
Sanity <stiannordholm@gmail.com>
2020-05-13 10:48:49 +0200

    Formatert

--- javascript/games.js
+++ javascript/games.js
@@ -13,7 +13,7 @@ function visSpill(snapshot) {
     let spill = snapshot.val();
     let spillNokkel = snapshot.key;
 
-    main.innerHTML +=   `
+    main.innerHTML += `
     <div id="card" class="cards" ><h3>${spill.Navn} </h3>
 			 <h3><img src="${spill.picture}" alt="${spill.Navn}" width="100%" height="100%"> <br> ${spill.Creator} </h3>
 	
